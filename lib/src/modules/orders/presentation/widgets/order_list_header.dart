c9b208465bbb349709a890d64afc167fcb4d238d
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-01-29 17:22:32 +0530

    chore: show current formatted date

--- lib/src/modules/orders/presentation/widgets/order_list_header.dart
+++ lib/src/modules/orders/presentation/widgets/order_list_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../core/theme/typography/typography.dart';
 
@@ -11,13 +12,15 @@ class OrdersHeader extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final fonts = ref.watch(fontsProvider);
 
+    final currentFormattedDate = DateFormat('MMMM yyyy').format(DateTime.now());
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: kToolbarHeight + 20),
         Text('Recent orders', style: fonts.heading2),
         const SizedBox(height: 12),
-        Text('May 2024', style: fonts.label2),
+        Text(currentFormattedDate, style: fonts.label2),
         const SizedBox(height: 12),
       ],
     );

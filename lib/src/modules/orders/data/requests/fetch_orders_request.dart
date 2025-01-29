952674f06f5ae80be22a4fbbe924afeeebf7bbf4
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-01-29 12:20:50 +0530

    chore: remove .dart_tools/

--- lib/src/modules/orders/data/requests/fetch_orders_request.dart
+++
@@ -1,9 +0,0 @@
-import 'package:meta/meta.dart';
-
-import '../../../../core/constants/api.dart';
-import '../../../../core/network/api_client.dart';
-
-@immutable
-class GetOrdersRequest extends BaseApiRequest {
-  GetOrdersRequest({super.endpoint = Api.orders});
-}
